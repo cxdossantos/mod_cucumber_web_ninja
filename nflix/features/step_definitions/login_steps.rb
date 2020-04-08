13fc6d218a7353dd4a06a53dc8c0b0bc9c749a9f
Cleiton Xavier <cxdossantos@gmail.com>
2020-04-08 20:24:34 -0300

    Validando login autenticado

--- nflix/features/step_definitions/login_steps.rb
+++ nflix/features/step_definitions/login_steps.rb
@@ -3,12 +3,13 @@ Quando("eu faço login com {string} e {string}") do |email, senha|
   find("#emailId").set email
   find("#passId").set senha
   click_button "Entrar"
-
   sleep 3
 end
 
 Então("devo ser autenticado") do
-  pending # Write code here that turns the phrase above into concrete actions
+  js_script = 'return window.localStorage.getItem("default_auth_token");'
+  token = page.execute_script(js_script)
+  expect(token.length).to be 147
 end
 
 Então("devo ver {string} na área logada") do |email|
